4db297b30dfdd55352f598f48a06c2aa577892a2
Maxime Kuntz <maxime.kuntz75@gmail.com>
2025-05-27 23:37:45 +0200

    Add colour palettes

---
+++ R/colours.R
@@ -0,0 +1,56 @@
+colours <- list(
+  news = list(
+    dark = "#ab0613",
+    main = "#c70000",
+    bright = "#ff4e36",
+    pastel = "#ffbac8",
+    faded = "#fff4f2"
+  ),
+  opinion = list(
+    dark = "#bd5318",
+    main = "#e05e00",
+    bright = "#ff7f0f",
+    pastel = "#f9b376",
+    faded = "#fef9f5"
+  ),
+  sport = list(
+    dark = "#005689",
+    main = "#0084c6",
+    bright = "#00b2ff",
+    pastel = "#90dcff",
+    faded = "#f1f8fc"
+  ),
+  culture = list(
+    dark = "#6b5840",
+    main = "#a1845c",
+    bright = "#eacca0",
+    pastel = "#e7d4b9",
+    faded = "#fbf6ef"
+  ),
+  lifestyle = list(
+    dark = "#7d0068",
+    main = "#bb3b80",
+    bright = "#ffabdb",
+    pastel = "#fec8d3",
+    faded = "#feeef7"
+  ),
+  brand = list(dark = "#041f4a", main = "#052962", pastel = "#506991"),
+  highlight = list(main = "#ffe500", dark = "#ffbb50"),
+  neutral = list(
+    brightness7 = "#121212",
+    brightness20 = "#333333",
+    brightness46 = "#767676",
+    brightness60 = "#999999",
+    brightness86 = "#dcdcdc",
+    brightness93 = "#ededed",
+    brightness97 = "#f6f6f6",
+    brightness10 = "#ffffff"
+  ),
+  other = list(
+    special_report_dark = "#3f464a",
+    labs_dark = "#4B8878",
+    labs_main = "#69d1ca",
+    green_dark = "#236925",
+    green_main = "#3db540"
+  )
+)
